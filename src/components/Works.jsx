cabb740f91ef2572cce4038ef360ebb8e74a0963
Nathan Rose <natetron99@gmail.com>
2023-11-23 06:19:25 -0500

    FIXED RUNNING SITE

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,3 @@
-import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -17,14 +16,6 @@ const ProjectCard = ({
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
-      >
         <div className='relative w-full h-[230px]'>
           <img
             src={image}
@@ -61,7 +52,6 @@ const ProjectCard = ({
             </p>
           ))}
         </div>
-      </Tilt>
     </motion.div>
   );
 };
